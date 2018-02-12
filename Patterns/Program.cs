310fca4d2be9732e12c225fb1dc88487603df8ca
Ilya <tefc@yandex.ru>
2018-02-12 18:05:12 +0300

    123

--- Patterns/Program.cs
+++ Patterns/Program.cs
@@ -6,7 +6,8 @@ namespace Patterns
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); 
         }
     }
 }
+ 
